26c3c14d1cc5a6114370900834b95bfd144c64ab
rajan singh <rajan.singh1@myoperator.co>
2021-01-17 00:11:44 +0530

    Handled events

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './styles/App.css';
 import React, { useEffect, useState } from 'react';
 import Box from './components/Box';
+import {directions} from './constants/constants';
 
 const App = () => {
 	const [ zIndex, setZIndex ] = useState(1);
@@ -23,12 +24,131 @@ const App = () => {
 		setBoxes((old) => [ ...old, box ]);
 	};
 
+	const handleKeyPressEvent = (event) => {
+		const boxId = selectedBox;
+		if (!boxId) {
+			return;
+		}
+		const key = event.key.toLowerCase();
+		console.log(key, boxId);
+		switch (key) {
+			case 'w':
+				moveObject(boxId, directions.UP);
+				break;
+			case 's':
+				moveObject(boxId, directions.DOWN);
+				break;
+			case 'a':
+				moveObject(boxId, directions.RIGHT);
+				break;
+			case 'd':
+				moveObject(boxId, directions.LEFT);
+				break;
+			case 'delete':
+				console.log('Here to delete', boxId);
+				setBoxes((boxes) => boxes.filter((box) => box.id !== boxId));
+			default:
+				break;
+		}
+	};
+
+	const moveObject = (boxId, direction) => {
+		switch (direction) {
+			case directions.UP:
+				setBoxes((boxes) =>
+					boxes.map((box) => {
+						if (box.id === boxId) {
+							const marginTop = box.marginTop - 1 > 0 ? box.marginTop - 1 : 0;
+							return {
+								...box,
+								marginTop
+							};
+						}
+						return { ...box };
+					})
+				);
+				break;
+
+			case directions.DOWN:
+				setBoxes((boxes) =>
+					boxes.map((box) => {
+						if (box.id === boxId) {
+							const marginTop =
+								box.marginTop + 1 < window.innerHeight - 400
+									? box.marginTop + 1
+									: window.innerHeight - 400;
+							return {
+								...box,
+								marginTop: marginTop
+							};
+						}
+						return { ...box };
+					})
+				);
+				break;
+
+			case directions.LEFT:
+				setBoxes((boxes) =>
+					boxes.map((box) => {
+						if (box.id === boxId) {
+							const marginLeft = box.marginLeft - 1 > 0 ? box.marginLeft - 1 : 0;
+							return {
+								...box,
+								marginLeft
+							};
+						}
+						return { ...box };
+					})
+				);
+				break;
+
+			case directions.RIGHT:
+				setBoxes((boxes) =>
+					boxes.map((box) => {
+						if (box.id === boxId) {
+							const marginLeft =
+								box.marginLeft + 1 < window.innerWidth - 400
+									? box.marginLeft + 1
+									: window.innerWidth - 400;
+							return {
+								...box,
+								marginLeft
+							};
+						}
+						return { ...box };
+					})
+				);
+				break;
+			default:
+				break;
+		}
+	};
+
+	useEffect(
+		() => {
+			document.addEventListener('keypress', handleKeyPressEvent);
+			return () => {
+				document.removeEventListener('keypress', handleKeyPressEvent);
+			};
+		},
+		[ selectedBox ]
+	);
 
 	return (
-		<div className="App">
+		<div className="App" style={{ height: '100vh', width: '100%' }}>
 			<div>
 				<button onClick={addNewBox}>Add box</button>
 			</div>
+			<div
+				style={{
+          margin: 'auto',
+          marginTop: '70px',
+          height: '70vh',
+					boxShadow: '0px 0px 20px #424242',
+          border: '2px splid blue',
+          width: `${window.innerWidth - 300}px`
+				}}
+			>
 				{boxes.map((item) => {
 					if (!item) return '';
 					return (
@@ -43,9 +163,11 @@ const App = () => {
 						/>
 					);
 				})}
+			</div>
 		</div>
 	);
 };
 
 export default App;
+
 
